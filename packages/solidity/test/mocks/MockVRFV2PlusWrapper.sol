fb11b1044311f006575580e2832e42c199eb0f2a
Jason T. <jason.tay.nw@u.nus.edu>
2025-11-09 17:08:26 +0800

    changed anonymous imports to named imports

--- packages/solidity/test/mocks/MockVRFV2PlusWrapper.sol
+++ packages/solidity/test/mocks/MockVRFV2PlusWrapper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../../contracts/VrfConsumer.sol";
+import {VrfConsumer, IVRFV2PlusWrapper} from "../../contracts/VrfConsumer.sol";
 
 /**
  * @title MockVRFV2PlusWrapper
