55e42ea5da1d702c3490c6c32798ee1c41d1a392
Jason T. <jason.tay.nw@u.nus.edu>
2025-11-10 01:57:56 +0800

    Install chainlink via npm instead of forge

--- packages/solidity/contracts/VrfConsumer.sol
+++ packages/solidity/contracts/VrfConsumer.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {IVRFV2PlusWrapper} from "@chainlink/contracts/src/v0.8/vrf/interfaces/IVRFV2PlusWrapper.sol";
+import {IVRFV2PlusWrapper} from "@chainlink/contracts/src/v0.8/vrf/dev/interfaces/IVRFV2PlusWrapper.sol";
 
 // ERC20 interface - minimal interface with only functions we use
 interface IERC20 {
