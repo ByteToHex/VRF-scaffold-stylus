e2f7818e8b71f5e83162cfacda257df5c37ddd8f
Jason T. <jason.tay.nw@u.nus.edu>
2025-11-09 17:06:46 +0800

    remove unused imports

--- packages/solidity/test/VrfConsumerE2E.t.sol
+++ packages/solidity/test/VrfConsumerE2E.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {ERC20Example} from "../contracts/ERC20Example.sol";
 import {VrfConsumer} from "../contracts/VrfConsumer.sol";
 import {MockVRFV2PlusWrapper} from "./mocks/MockVRFV2PlusWrapper.sol";
